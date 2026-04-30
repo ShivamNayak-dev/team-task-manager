40c1fe973723fb4ea79463f83a1b03d3858e2039
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-04-30 17:45:39 +0530

    update API URL to Railway backend

--- task-manager-frontend/task-manager-frontend/src/services/api.js
+++ task-manager-frontend/task-manager-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://team-task-manager-production-9bcf.up.railway.app'
 
 const api = axios.create({ baseURL: BASE_URL })
 
